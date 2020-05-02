cfb763ebb133bf64b70f002fa8e86464040b2164
ruspen <rlevitskiywork@gmail.com>
2020-05-02 18:21:14 +0300

    add Tetris Mechanics

---
+++ Tetris/Assets/Tetris/GameModule/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tetris.GameModule
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject[] Groups;
+
+
+        public void SpawnNext()
+        {
+            int i = Random.Range(0, Groups.Length);
+
+            // Spawn Group at current Position
+            Instantiate(Groups[i], transform.position, Quaternion.identity);
+        }
+
+
+        void Start()
+        {
+            SpawnNext();
+        }
+
+        
+        void Update()
+        {
+
+        }
+    }
+}
+
